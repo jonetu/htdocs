d3d51ed8fce2287f91b86cfe776e15d4ba337f8d
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-12-02 17:42:14 -0300

    primeiros passos

---
+++ php_basico/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+  <head>
+    <title>test</title>
+    <meta charset="utf-8">
+  </head>
+  
+  <body>
+    <p>
+    <pre
+        <?php 
+        
+        echo $_SERVER['SERVER_NAME'];
+        print_r($_SERVER);
+        
+        ?>
+
+        ></pre>
+        
+    </p>
+  </body>
+</html>
