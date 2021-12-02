8bf0a212c57b9b3a0c7f32095538dc445f2f776c
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-12-02 18:58:23 -0300

    constantes e concatenacao

--- php_basico/index.php
+++ php_basico/index.php
@@ -13,9 +13,34 @@
         echo $_SERVER['SERVER_NAME'];
         print_r($_SERVER);
         
+        $variavel = "olá mundo";
+        echo $variavel;
+        echo '<br/>';
+        $name = "João";
+        echo 'Olá '.$name.', bem vindo.';
+
+        $inteiro = 10;
+        $booleano = true;
+        $float = 10.5234324;
+
+        define('CONSTANTE','<br/>nunca altera <br/>');
+        echo CONSTANTE;
+        
+        $meu_array = array('name1','name2','name3');
+        $meu_array2 = ['name1','name2','name3'];
+
+        $dicionario['nome'] = 'João';
+        $dicionario['idade'] = 20;
+        $dicionario['país'] = 'Brasil';
+
+        echo $dicionario['nome'].', '.$dicionario['idade'].' anos.';
+
+        $cor_da_div = 'yellow';
+        //concatenacao com '\'
+        echo "<div style=\"background-color:$cor_da_div\"> conteúdo concatenado</div> ";
         ?>
 
-        ></pre>
+        </pre>
         
     </p>
   </body>
