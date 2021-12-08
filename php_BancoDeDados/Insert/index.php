417402ee88a97d87d76980170b0e9ef2b46ac353
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-12-08 15:31:27 -0300

    insert no banco de dados

---
+++ php_BancoDeDados/Insert/index.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * OBS: Banco de dados mysql criado na maquina local com informacoes padroes, 
+ * nome da base de dados = mybd; 
+ * tabela clientes criada com informacoes sobre: id(auto increment), nome varchar, email varchar, senha varchar;
+ * 
+ * 
+ */
+$pdo = new PDO('mysql:host=localhost;dbname=mybd', 'root', '');
+//Metodo direto --SEM SEGURANCA DE TAGS MALICIOSAS --
+// $nome = $_POST['nome'];
+//$email = $_POST['email'];
+//$senha = $_POST['senha'];
+// $sql = $pdo->prepare("INSERT INTO `clientes` (`id`, `nome`, `email`, `senha`) VALUES (NULL, '".$nome."', '".$email."', '".$senha."')");
+
+if (isset($_POST['cliente'])) {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    if ($nome != '' && $email != '' && $senha != '') {
+        $sql = $pdo->prepare("INSERT INTO `clientes` (`id`, `nome`, `email`, `senha`) VALUES (NULL,?,?,?)");
+        $sql->execute(array($nome, $email, $senha));
+        echo "Enviado para o banco de dados!";
+    }
+}
+
+?>
+<!doctype html>
+<html>
+
+<head>
+    <title>Enviar dados</title>
+</head>
+
+<body>
+    <h1>Cadastro no banco de dados</h1>
+    <form method="post">
+        <input type="text" name="nome" placeholder="nome" />
+        <input type="text" name="email" placeholder="email" />
+        <input type="password" name="senha" placeholder="senha" />
+        <input type="submit" name="cliente" valie="enviar!" />
+    </form>
+
+</body>
+
+</html>
