2cf5bd4950408d3d7f16f71f05d95ae5b5d7850a
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-12-09 09:43:07 -0300

    select banco de dados

---
+++ php_BancoDeDados/select/index.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * OBS: Banco de dados mysql criado na maquina local com informacoes padroes, 
+ * nome da base de dados = mybd; 
+ * tabela clientes criada com informacoes sobre: id(auto increment), nome varchar, email varchar, senha varchar;
+ * 
+ * 
+ */
+$pdo = new PDO('mysql:host=localhost;dbname=mybd', 'root', '');
+        $sql = $pdo->prepare("SELECT * from clientes");
+        $sql->execute();
+        $info = $sql->fetchAll();
+        echo "select dos dados";
+
+?>
+<!doctype html>
+<html>
+
+<head>
+    <title>SELECT</title>
+</head>
+
+<body>
+    <h1>Dados cadastrados:</h1>
+    <pre>
+        <?php
+        print_r($info);
+
+        echo '<hr>';
+        foreach($info as $key => $value){
+            echo " ID: ".$info[$key]['id'];
+            echo '<br/>';
+            echo " Nome: ".$info[$key]['nome'];
+            echo '<br/>';
+            echo " Email: ".$info[$key]['email'];
+            echo '<br/>';
+            echo " senha: ".$info[$key]['senha'];
+            echo '<br/>';
+            echo '<hr>';
+        }
+
+        ?>
+    </pre>
+</body>
+
+</html>
