2bbf50b27b9bb9623894be0285b7016315451d6b
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-12-09 11:09:09 -0300

    PDO sem mostrar password em Exception connect

--- php_BancoDeDados/PDO/index.php
+++ php_BancoDeDados/PDO/index.php
@@ -1,6 +1,17 @@
 <?php
+    define('HOST','localhost');
+    define('DATABASE','mybd');
+    define('USER', 'root');
+    define('PASSWORD','');
 
-    $pdo = new PDO('mysql:host=localhost;dbname=mybd','root','');
+    try {
+        $pdo = new PDO('mysql:host='.HOST.';dbname='.DATABASE.'',USER,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        echo 'conectado com sucesso';
+    } catch (Exception $e) {
+        echo 'erro ao conectar';
+    }
+    
     
 
 ?>
