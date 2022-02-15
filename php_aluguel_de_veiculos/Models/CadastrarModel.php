bc20e5dd64ecfd3d9415b0b96dcce91186a175a6
joaorodriguesrd <j_neto_rodrigues@hotmail.com>
2022-02-15 19:56:03 -0300

    refatorando model cadastrar

--- php_aluguel_de_veiculos/Models/CadastrarModel.php
+++ php_aluguel_de_veiculos/Models/CadastrarModel.php
@@ -11,19 +11,10 @@ class CadastrarModel{
  * @return FALSE Se ja estiver cadastrado.
  * @return TRUE Se tiver sucesso em cadastrar.
  */ 
-    //true - > Sucesso no cadastro
-    //false - > Falha no cadastro
     public static function cadastrar($placa, $marca, $modelo,$precoDia){
         $pdo = new PDO('mysql:host=localhost;dbname=locadora', 'root', '');
         $sql = $pdo->prepare("INSERT INTO `carro` (`placa`, `marca`, `modelo`, `precoDia`) VALUES (?,?,?,?)");
-        //$sql->execute(array($placa, $marca, $modelo,$precoDia));
-        if($sql->execute(array($placa, $marca, $modelo,$precoDia))){
-            echo "TRUE";
-        }else{
-
-            echo "FALSE";
-        }
-        return true;
+        return $sql->execute(array($placa, $marca, $modelo,$precoDia));
     }
 
 }
