5021a4c2e70445574e7b9351c44d5f758283012a
joaorodriguesrd <j_neto_rodrigues@hotmail.com>
2022-02-16 15:41:17 -0300

    forms de cadastrar cliente

--- php_aluguel_de_veiculos/Views/pages/cadastroCliente.php
+++ php_aluguel_de_veiculos/Views/pages/cadastroCliente.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-<title>Cadastro</title>
-</head>
-
 <body>
-    <h1 class="center">
-        <?php echo $parameter['titulo']?>!
-    </h1>
-</body>
+    <h3 class="container d-flex justify-content-center">
+        Cadastro de Clientes
+    </h3>
 
+
+    <div class ="container">
+    <form method="post">
+  <div class="form-group">
+    <label for="exampleFormControlInput1" >Nome completo</label>
+    <input type="text" class="form-control" name="nome" required placeholder="Ex: Maria Antonia" maxlength="100">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">cpf</label>
+    <input type="text" class="form-control" name="cpf" placeholder="Formato: 12312312312" maxlength="11">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">email</label>
+    <input type="text" class="form-control" name="email" placeholder="exemplo@exemplo.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Telefone de contato</label>
+    <input type="number" class="form-control" name="telefone" placeholder="Formato: 86912341234">
+  </div>
+  <button type="submit" name="action" class="btn btn-success">Cadastrar Cliente</button>
+</form>
+    </div>
+</body>
+<!-- Script para evitar reenvio do form -->
+<script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
 </html>
